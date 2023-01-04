6288630c5c633d373d7d81abbe46cc408515bdc0
nightrord <zhongpeizhang@gmail.com>
2023-01-04 10:09:37 -0500

    create new project

---
+++ src/TodoItem.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class TodoItem extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	render() {
+		const {content, test} = this.props
+		return (
+			<div 
+				onClick={this.handleClick}>{test}-{content}
+			</div>
+		)
+	}
+
+	handleClick() {
+		const {deleteItem, index } = this.props;
+		deleteItem(index)
+	}
+}
+
+TodoItem.propTypes = {
+	content: PropTypes.string,
+	deleteItem: PropTypes.func,
+	index: PropTypes.number,
+	test: PropTypes.string.isRequired
+}
+
+TodoItem.defaultProps = {
+	test: 'hello'
+}
+
+export default TodoItem;
